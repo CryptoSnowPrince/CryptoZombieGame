8cf3a368769677fa138b45b405ff282b2387d9c4
DESKTOP-3J65I2E\admin <RogerDTilton@gmail.com>
2021-12-07 22:31:44 +0300

    All files of the Project are committed.

---
+++ src/components/CryptoZombies.js
@@ -0,0 +1,241 @@
+import { useWeb3React } from "@web3-react/core";
+import { useState, useEffect, useRef } from "react";
+// import { ABI, address } from "./config.js";
+import { Typography, Button } from "@mui/material";
+import ZombieCard from "./ZombieCard.js";
+import Box from "@mui/material/Box";
+import status from "./ZombieStatus";
+
+// custom+ start
+const ABI = require("../contracts/ZombieOwnership.json").abi;
+const address = "Your_Contract_Address";
+// custom+ end
+
+function CryptoZombies() { 
+  const { account, library } = useWeb3React();
+  let cryptoZombies = new library.eth.Contract(ABI, address);
+  
+  const [isFetchingArmy, setIsFetchingArmy] = useState(true);
+  const [army, setArmy] = useState([]);
+
+  function getZombiesByOwner(owner) {
+    return cryptoZombies.methods.getZombiesByOwner(owner).call();
+  }
+
+  const refreshArmy = useRef(() => {});
+  refreshArmy.current = (account) => {
+    setIsFetchingArmy(true);
+    setArmy([]);
+    getZombiesByOwner(account).then((zombieIds) => {
+      console.log("hello");
+      // console.log({ zombieIds });
+      console.log(zombieIds.length);
+      if (zombieIds.length === 0) {
+        // custom +
+        console.log("pass if");
+        createRandomZombie("CryptoZombie" + Math.floor((Math.random() * 1000000000).toString(8)));
+        // custom -
+      }
+      console.log("pass else");
+      // zombieIds.ABI.zombies.
+      zombieIds.forEach((zombieId) => {
+        getZombieDetails(zombieId).then((zombieDetails) => {
+          console.log({zombieDetails});
+          zombieDetails.id = zombieId;
+          zombieDetails.status = status.READY;
+          setArmy((prevArmy) => [...prevArmy, zombieDetails]);
+          setIsFetchingArmy(false);
+        });
+      });
+      
+    });
+  };
+
+  useEffect(() => {
+    refreshArmy.current(account);
+  }, [account, refreshArmy]);
+
+  // custom +
+  function createRandomZombie(name) {
+    console.log("createRandomZombie");
+    // This is going to take a while, so update the UI to let the user know
+    // the transaction has been sent
+    console.log("Creating new zombie on the blockchain. This may take a while...");
+  
+    cryptoZombies.deployed().then(function(instance){
+      return instance.createRandomZombie(name, { from: account });
+    }).then(function(receipt){
+      console.log("Successfully created " + name + "!");
+  
+      // Transaction was accepted into the blockchain, let's redraw the UI
+      // getZombiesByOwner(account).then(displayZombies); 
+    }).catch(function(err) {
+      console.log(err.message);
+    });
+  }
+  // custom -
+  function getZombieDetails(id) {
+    return cryptoZombies.methods.zombies(id).call();
+  }
+
+  function createRandomZombie(name = "NoName") {
+    // This is going to take a while, so update the UI to let the user know
+    // the transaction has been sent
+    return cryptoZombies.methods
+      .createRandomZombie(name)
+      .send({ from: account })
+      .on("receipt", function (receipt) {
+        console.log({ receipt });
+        console.log("Successfully created " + name + "!");
+        // Transaction was accepted into the blockchain, let's redraw the UI
+        refreshArmy.current(account);
+      })
+      .on("error", function (error) {
+        console.error({ error });
+        // Do something to alert the user their transaction has failed
+      });
+  }
+  // function zombieToOwner(id) {
+  //   return cryptoZombies.methods.zombieToOwner(id).call()
+  // }
+  function updateZombieAttributes(newZombieObj) {
+    setArmy((prevArmy) => {
+      return prevArmy.map(z => {
+        if(z.id === newZombieObj.id){
+          z = newZombieObj;
+        }
+        return z;
+      })
+    });
+  }
+
+  const levelUp = (zombie) => {
+    console.log('leveling up...');
+    updateZombieAttributes({...zombie, status: status.LEVELING_UP});
+    return cryptoZombies.methods
+      .levelUp(zombie.id)
+      .send({ from: account, value: library.utils.toWei("0.001", "ether") })
+      .on("receipt", function (receipt) {
+        console.log({ receipt });
+        console.log("Successfully leveled up!");
+        // Transaction was accepted into the blockchain, let's redraw the UI
+        updateZombieAttributes({...zombie, level: parseInt(zombie.level)+1, status: status.READY});
+      })
+      .on("error", function (error) {
+        console.error({ error });
+        // Do something to alert the user their transaction has failed
+        console.log('failed');
+        updateZombieAttributes({...zombie, status: status.READY});
+      });
+  }
+
+  const changeName = (zombie, newName) => {
+    console.log('changing name...');
+    updateZombieAttributes({...zombie, status: status.CHANGING_NAME});
+    return cryptoZombies.methods
+      .changeName(zombie.id, newName)
+      .send({ from: account })
+      .on("receipt", function (receipt) {
+        console.log({ receipt });
+        console.log("Successfully changed name!");
+        // Transaction was accepted into the blockchain, let's redraw the UI
+        updateZombieAttributes({...zombie, name: newName, status: status.READY});
+      })
+      .on("error", function (error) {
+        console.error({ error });
+        // Do something to alert the user their transaction has failed
+        console.log('failed');
+        updateZombieAttributes({...zombie, status: status.READY});
+      });
+  }
+
+  const attackRandomEnemyZombie = async (zombie) => {
+    updateZombieAttributes({...zombie, status: status.ATTACKING});
+    // custom +
+    // Get total number of zombies
+    const numZombies = await cryptoZombies.methods.totalZombies.call((e, num) => {
+      return num;
+    });
+    // Get a random zombie until the owner of that zombie is not the current user
+    let targetZombieOwner = null;
+    let randZombieId = 3;//null;
+    do {
+      randZombieId = Math.floor(Math.random() * numZombies);
+      targetZombieOwner = await cryptoZombies.methods.zombieToOwner(randZombieId).call((e, owner) => {
+        return owner;
+      });
+    }
+    while (targetZombieOwner === account);
+
+    // Attack that random enemy zombie
+    // custom -
+    return cryptoZombies.methods
+    .attack(zombie.id, randZombieId)
+    .send({ from: account })
+    .on("receipt", function (receipt) {
+      console.log({ receipt });
+      console.log("Successfully attacked zombie!");
+      // Transaction was accepted into the blockchain, let's redraw the UI
+      refreshArmy.current(account);
+    })
+    .on("error", function (error) {
+      console.error({ error });
+      // Do something to alert the user their transaction has failed
+      console.log('failed');
+      updateZombieAttributes({...zombie, status: status.READY});
+    });
+  }
+
+  const renderedArmy = army.map((zombie) => (
+    <Box key={zombie.dna} sx={{ m: 2 }}>
+      <ZombieCard zombie={zombie} levelUp={levelUp} changeName={changeName} attackRandomEnemyZombie={attackRandomEnemyZombie} />
+    </Box>
+  ));
+
+  return (
+    <div>
+      {isFetchingArmy ? (
+        <Typography variant="h2" align="center">
+          Loading...
+        </Typography>
+      ) : (
+        ""
+      )}
+      {!isFetchingArmy && army.length ? (
+        <div>
+          <Typography
+            variant="h3"
+            align="center"
+            style={{ marginBottom: "5vh", textTransform: "uppercase" }}
+          >
+            Your troops
+          </Typography>
+
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+            }}
+          >
+            {renderedArmy}
+          </Box>
+        </div>
+      ) : (
+        ""
+      )}
+      {!isFetchingArmy && !army.length ? (
+        <div>
+          <Typography variant="h2">You have no zombies</Typography>
+          <Button onClick={() => createRandomZombie()}>
+            Give me a random Zombie
+          </Button>
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
+
+export default CryptoZombies;
---
+++ src/components/config.js
@@ -0,0 +1,6 @@
+// const ZombieOwnership = require("../contracts/ZombieOwnership.json");
+// const ABI = ZombieOwnership.abi;
+// const address = "Your Contract Address";
+
+// export default {ABI};
+// export {address};
